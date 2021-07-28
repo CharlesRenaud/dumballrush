bd7f6aac61ce24275ea25d914f8e9145d9602fb2
Charles Renaud <renaudcharlespro@gmail.com>
2021-07-28 14:58:31 +0200

    test yt

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import backgroundImage from '../images/background.png';
 import intro from '../images/intro.gif';
-import steam from '../images/steam2.png'
+import steam from '../images/steam2.png';
+
 
 import "../styles/homepage.scss";
 
@@ -10,7 +11,9 @@ const Homepage = () => {
     return (
     <div id='background' style={{ backgroundImage: `url(${backgroundImage})` }}>
         <div id="trailer">
-            <img id="trailer-img" src={intro} ></img>
+           
+            <iframe width="350" height="360" src="https://www.youtube.com/watch?v=8jjpDWoFMB0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
             <div style={{ backgroundImage: `url(${backgroundImage})` }} id="add-steam"> <a id="link-steam">add to wishlist <img id="steam-logo" src={steam}></img></a></div>
         </div>
     </div>
